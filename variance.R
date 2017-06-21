d1fd28682464503abbeeccb713f094bcf3855446
Daniel Maxwell <dsm_direct@yahoo.com>
2017-06-21 18:51:47 -0400

    Added code to place the four plots in a 2 x 2 matrix.

--- variance.R
+++ variance.R
@@ -7,22 +7,29 @@
 # demonstrated in the code below.  However, the effect will be much harder to
 # detect when outliers are present as the variance is higher.
 
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(2, 2))
+
 # Generate a set of 100 random numbers from a normal distribution.
 tst <- rnorm(n = 100, mean = 0, sd = 1)
 
 # Generate a histogram of the original vector.
 hist(tst, breaks = seq(from = -3, to = 3, by = 1), 
+          main   = "Histogram",
           xlim   = range(-3:3),
           col    = "lightgreen")
 
 # Generate a density plot of the vector and add a curve to it.
-hist(tst, freq = FALSE, main = "Density", col = "lightgreen")
+hist(tst, freq = FALSE, 
+     breaks = seq(from = -3, to = 3, by = 1),
+     main   = "Density", 
+     col    = "lightgreen")
 
 curve(dnorm(x, mean = mean(tst), sd = sd(tst)), 
       add = TRUE, 
       col = "darkblue", 
       lwd = 1)
-
+ 
 var(tst)              # Check variance.
 
 tst[101] <- 10        # Add an outlier.
@@ -31,15 +38,20 @@ var(tst)              # Check variance.
 
 # Generate a histogram of the modified vector.
 hist(tst, breaks = seq(from = -3, to = 10, by = 1), 
+          main   = "Histogram",
           xlim   = range(-3:10),
           col    = "lightgreen")
 
 # Generate a density plot of the modified vector and add a curve to it.
-hist(tst, freq = FALSE, main = "Density", col = "lightgreen")
+hist(tst, freq = FALSE, 
+     breaks = seq(from = -3, to = 10, by = 1),
+     main   = "Density", 
+     col    = "lightgreen")
 
 curve(dnorm(x, mean = mean(tst), sd = sd(tst)), 
       add = TRUE, 
       col = "darkblue", 
       lwd = 1)
 
-
+par(opar)
+ 
