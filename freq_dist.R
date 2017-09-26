b56ed9bc1b1071c503133b9d0eb865a1a6b8aa0c
Daniel Maxwell <dsm_direct@yahoo.com>
2017-09-25 21:08:05 -0400

    Added code for problem 19 (p. 65).

--- freq_dist.R
+++ freq_dist.R
@@ -1,4 +1,6 @@
-v <- c(1,2,2,2,3,3,3,4,4,5)
+# Page 37 dataset.
+
+r <- c(1,2,2,2,3,3,3,4,4,5)
 X <- c(5,4,3,2,1)
 f <- c(1,2,3,3,1)
 
@@ -14,5 +16,47 @@ colnames(df) <- c("X","f","fX","p","cp")
 
 # Plot the histogram, using the raw dataset.
 
-hist(v, breaks = c(0,1,2,3,4,5), col = "lightblue", main = "Histogram")
+hist(r, breaks = c(0,1,2,3,4,5), col = "lightblue", main = "Histogram")
+
+# ---------------------------------------------------------------------------
+# Answer to Problem 19.
+
+r  <- c(5,5,6,6,6,6,6,7,7,7,7,7,7,8,8,8)
+X  <- c(8,7,6,5)
+f  <- c(3,6,5,2)
+cf <- c(18,15,9,4)
+cp <- c(72,60,36,16)
+
+# Calculate the percentile rank for X = 6
+y <- .5 * (cp[3] - cp[4])
+cp[3] - y
+
+# Calculate the percentile rank for X = 7
+y <- .5 * (cp[2] - cp[3])
+cp[2] - y
+
+# Calculate the 20th percentile
+numerator   <- cp[3] - 20
+denominator <- cp[3] - cp[4]
+
+pct <- numerator/denominator  
+
+# Multiply 1 by the percent because there is a 1 point
+# difference between the X values.  Add .5 to the upper
+# X value to account for bin width.
+
+6.5 - (1 * pct)
+
+# Calculate the 66th percentile
+numerator   <- cp[1] - 66
+denominator <- cp[1] - cp[2]
+
+pct <- numerator/denominator  
+
+# Multiply 1 by the percent because there is a 1 point
+# difference between the X values.  Add .5 to the upper
+# X value to account for bin width.
+
+8.5 - (1 * pct)
+
 
