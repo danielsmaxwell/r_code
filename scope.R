72f4f688eb7ae3a6a938dfcd43dfd7fa5aa109e1
Daniel Maxwell <dsm_direct@yahoo.com>
2019-05-31 12:16:01 -0400

    Added documentation.

--- scope.R
+++ scope.R
@@ -1,9 +1,14 @@
+# Create a function that uses the global assignment operator to change a variable's value outside of its scope.
+
 set_var <- function() {
   a_var <<- 25 
 }
 
+# Assign a value to a test variable
 a_var <- 30
 
+# Call the set_var() function...
 set_var()
 
+# ... and set_var() changes the global variable's value.
 a_var
